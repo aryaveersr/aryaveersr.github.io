b4c1f578de758ef017cf66b10d3c547c4752f03c
aryaveersr <me.aryaveer@gmail.com>
2026-08-22 00:05:41 +0530

    markdown: use `satteri-shift-headings` plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,14 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
 import { satteri } from "@astrojs/markdown-satteri";
-import { shiftHeadings } from "@plugins/shift-headings";
+import { shiftHeadings } from "satteri-shift-headings";
 
 export default defineConfig({
   vite: { plugins: [tailwindcss()] },
   integrations: [icon()],
   markdown: {
     processor: satteri({
-      mdastPlugins: [shiftHeadings],
+      mdastPlugins: [shiftHeadings(1)],
     }),
   },
 });
--- src/plugins/shift-headings.ts
+++
@@ -1,9 +0,0 @@
-import { defineMdastPlugin } from "satteri";
-
-export const shiftHeadings = defineMdastPlugin({
-  name: "shift-headings",
-  heading(node, ctx): void {
-    const depth = node.depth == 6 ? 6 : ((node.depth + 1) as 2 | 3 | 4 | 5 | 6);
-    ctx.setProperty(node, "depth", depth);
-  },
-});
