c7f1b8ae1a3b97e2d41e8c03e6a284c45887eb21
aryaveersr <me.aryaveer@gmail.com>
2026-08-26 14:21:20 +0530

    fix eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ import ts from "typescript-eslint";
 import eslintPluginAstro from "eslint-plugin-astro";
 
 export default [
-  ...js.configs.recommended,
+  js.configs.recommended,
   ...ts.configs.recommended,
   ...eslintPluginAstro.configs.recommended,
   {
